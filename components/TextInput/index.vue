6f32487358088300d8bcb8f4a01699a6c97e52b3
KeziahMoselle <keziah.moselle@gmail.com>
2019-11-17 21:22:40 +0100

    feat: styling error success

--- components/TextInput/index.vue
+++ components/TextInput/index.vue
@@ -1,64 +1,89 @@
 <template>
-  <div @keyup.enter="login" class="flex">
-    <input
-      v-model="studentId"
-      class="input rounded-l-full py-2 pl-5 border-gray-400 focus:outline-none"
-      type="text"
-      placeholder="Entrez votre numéro étudiant"
-    />
-    <div
-      @click="login"
-      class="px-3 rounded-r-full flex justify-center items-center bg-white cursor-pointer"
-    >
-      <transition name="fade" mode="out-in">
-        <ArrowRightIcon
-          key="idle"
-          v-if="state === 'idle'"
-          size="1.5x"
-          class="text-black"
-        />
-        <LoaderIcon
-          key="loading"
-          v-if="state === 'loading'"
-          size="1.5x"
-          class="text-black"
-        />
-        <XIcon
-          key="error"
-          v-if="state === 'error'"
-          size="1.5x"
-          class="text-black"
-        />
-        <CheckCircleIcon
-          key="success"
-          v-if="state === 'success'"
-          size="1.5x"
-          class="text-black"
-        />
-      </transition>
+  <div>
+    <div @keyup.enter="login" class="flex">
+      <input
+        v-model="studentId"
+        :class="
+          `input rounded-l-full py-2 pl-5 border-gray-400 focus:outline-none bg-${state}`
+        "
+        type="text"
+        placeholder="Entrez votre numéro étudiant"
+      />
+      <div
+        @click="login"
+        :class="
+          `px-3 rounded-r-full flex justify-center items-center bg-white cursor-pointer bg-${state}`
+        "
+      >
+        <transition name="fade" mode="out-in">
+          <ArrowRightIcon
+            key="idle"
+            v-if="state === 'idle'"
+            size="1.5x"
+            class="text-black"
+          />
+          <LoaderIcon
+            key="loading"
+            v-if="state === 'loading'"
+            size="1.5x"
+            class="text-black"
+          />
+          <XIcon
+            key="error"
+            v-if="state === 'error'"
+            size="1.5x"
+            class="text-black"
+          />
+          <CheckIcon
+            key="success"
+            v-if="state === 'success'"
+            size="1.5x"
+            class="text-black"
+          />
+        </transition>
+      </div>
     </div>
+
+    <p
+      v-if="state === 'error' || state === 'success'"
+      :class="`flex items-center mt-2 ${state}`"
+    >
+      <CheckIcon
+        v-if="state === 'success'"
+        :class="`text-${state}`"
+        size="1x"
+      />
+      <XIcon v-if="state === 'error'" :class="`text-${state}`" size="1x" />
+      <span :class="`ml-2 text-${state}`">{{ messages[state] }}</span>
+    </p>
   </div>
 </template>
 
 <script>
-import {
-  ArrowRightIcon,
-  LoaderIcon,
-  XIcon,
-  CheckCircleIcon
-} from 'vue-feather-icons'
+import { ArrowRightIcon, LoaderIcon, XIcon, CheckIcon } from 'vue-feather-icons'
 
 export default {
   components: {
     ArrowRightIcon,
     LoaderIcon,
     XIcon,
-    CheckCircleIcon
+    CheckIcon
   },
   data() {
     return {
       studentId: '',
-      state: 'idle'
+      state: 'idle',
+      messages: {
+        error: "Ce numéro n'existe pas",
+        success: 'Ce numéro existe'
+      }
+    }
+  },
+  watch: {
+    studentId(value) {
+      if (value === '') {
+        this.state = 'idle'
+      }
     }
   },
   methods: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        error: '#FE4C6A',
+        success: '#2BE8C6'
+      }
+    }
+  },
   variants: {},
   plugins: []
 }
