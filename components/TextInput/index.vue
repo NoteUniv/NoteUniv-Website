e0351b826c42787a513b42b0ad25b0159e64ed86
KeziahMoselle <keziah.moselle@gmail.com>
2019-11-17 20:36:09 +0100

    feat: text input component

--- components/TextInput/index.vue
+++ components/TextInput/index.vue
@@ -1,14 +1,46 @@
 <template>
-  <input class="input" type="text" placeholder="Entrez votre numéro étudiant" />
+  <div @keyup.enter="login" class="flex">
+    <input
+      v-model="studentId"
+      class="input rounded-l-full py-2 pl-5 border-gray-400 focus:outline-none"
+      type="text"
+      placeholder="Entrez votre numéro étudiant"
+    />
+    <div
+      @click="login"
+      class="px-3 rounded-r-full flex justify-center items-center bg-white cursor-pointer"
+    >
+      <ArrowRightIcon size="1.5x" class="text-black" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { ArrowRightIcon } from 'vue-feather-icons'
+
+export default {
+  components: {
+    ArrowRightIcon
+  },
+  data() {
+    return {
+      studentId: ''
+    }
+  },
+  methods: {
+    login() {
+      fetch('/')
+        .then(() => console.log('yes'))
+        .catch(() => console.log('no'))
+    }
+  }
+}
 </script>
 
 <style>
 .input {
-  width: 300px;
-  @apply rounded-full py-2 px-5 border-gray-400 border-2;
+  width: 380px;
+  font-size: 25px;
+  color: #110133;
 }
 </style>
