6ac080dce9bd702bb63955ab02f59f32ec6e0f7e
YnothnaFr <45896947+YnothnaFr@users.noreply.github.com>
2019-12-21 17:40:16 +0100

    Commencement du dev du panel
    le panel.html est le style, qui sera intégré au panel.php par la suite. Pour un soucis de facilité j'ai choisis de d'abord le faire en HTML avant de l'intégré en PHP. Pareil pour le stylePanel.css.

--- panel.php
+++ panel.php
@@ -1,3 +1,22 @@
+<?php
+// require "vendor/autoload.php";
+// // recupération des variables d'environnement
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
+// $servername = getenv('SERVERNAME');
+// $dbname = getenv('DBNAME');
+// $username = getenv('USERNAME');
+// $password = getenv('PASSWORD');
+// //Connection bdd
+//     try {
+//         $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+//         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     } catch (PDOException $e) {
+//         echo "Connection failed: " . $e->getMessage();
+//     }
+// //Récupération Numéro Etudiant du formulaire
+// $id_etu = $_POST["numEtu"];
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -10,29 +29,20 @@
 
 <body>
     <?php
-    include("pwd.php");
 
-    $id_etu = $_POST["numEtu"];
+    // $sql_all_notes = "SELECT name_pdf FROM global";
+    // $list_notes = $bdd->query($sql_all_notes);
+    // $totalNote = []; // tableau de toutes les notes de l'élève
+    // while ($note = $list_notes->fetch()) { // note = matière + date (nom du PDF)
+    //     $sql_note = "SELECT note_etu FROM $note[0] WHERE id_etu = $id_etu";
+    //     $my_note = $bdd->query($sql_note);
+    //     $noteEtudiant = $my_note->fetch();
 
-    try {
-        $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-
-    $sql_all_notes = "SELECT name_pdf FROM global";
-    $list_notes = $bdd->query($sql_all_notes);
-    $totalNote = []; // tableau de toutes les notes
-    while ($note = $list_notes->fetch()) {
-        $sql_note = "SELECT note_etu FROM $note[0] WHERE id_etu = $id_etu";
-        $my_note = $bdd->query($sql_note);
-        $mynote = $my_note->fetch();
-        echo $note[0] . " -> " . $mynote[0] . "<br>";
-        array_push($totalNote, $mynote[0]);
-    }
-    $moyenne = array_sum($totalNote) / count($totalNote);
-    echo "<br> <p> Votre Moyenne est de : <strong> " . $moyenne . "</strong>";
+    //     echo $note[0] . " -> " . $noteEtudiant[0] . "<br>";
+    //     array_push($totalNote, $noteEtudiant[0]); // push de ces notes dans le tableau pour moyenne
+    // }
+    // $moyenne = array_sum($totalNote) / count($totalNote); // on fait la moyenne : Ensemble des notes du tableau / nbr de note
+    // echo "<br> <p> Votre Moyenne est de : <strong> " . $moyenne . "</strong>";
     ?>
 </body>
 
