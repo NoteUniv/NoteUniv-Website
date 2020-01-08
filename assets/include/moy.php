a93bec544b56d4922de98738dfef3f09cd00a0fd
YnothnaFr <45896947+YnothnaFr@users.noreply.github.com>
2020-01-08 19:36:34 +0100

    Accurate Average

--- assets/include/moy.php
+++ assets/include/moy.php
@@ -1,5 +1,5 @@
 <?php
-$sql_all_notes = "SELECT name_pdf, mini, note_coeff FROM global";
+$sql_all_notes = "SELECT name_pdf, mini, note_coeff, type_note FROM global";
 $list_notes = $bdd->query($sql_all_notes);
 $totalNote = []; // tableau de toutes les notes de l'élève
 $totalCoeff = [];
@@ -7,7 +7,7 @@ while ($note = $list_notes->fetch()) { // note = matière + date (nom du PDF)
     $sqlNote = "SELECT note_etu FROM $note[0] WHERE id_etu = $id_etu";
     $myNote = $bdd->query($sqlNote);
     $noteEtudiant = $myNote->fetch();
-    if ($noteEtudiant[0] > $note[1]) {
+    if ($noteEtudiant[0] > $note[1] && $note['type_note'] == "Note unique") {
         $noteEtudiant = $noteEtudiant[0] * $note["note_coeff"];
         array_push($totalNote, $noteEtudiant); // push de ces notes dans le tableau pour moyenne
         array_push($totalCoeff, $note["note_coeff"]);
