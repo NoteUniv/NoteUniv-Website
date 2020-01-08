7cc915db97ce8211ae4386e3233592f89d4ae7b0
KeziahMoselle <keziah.moselle@gmail.com>
2020-01-08 14:25:44 +0100

    refactor: prefer const, listen on input not document, fetch instead of jquery ajax

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,12 @@
-var NumEtu = localStorage.getItem("NumEtu");
+const numEtuInput = document.getElementById("numEtu")
+
+const NumEtu = localStorage.getItem("NumEtu");
 if (NumEtu !== null) {
-    document.getElementById("numEtu").value = NumEtu;
+    numEtuInput.value = NumEtu;
 }
 
 function submitForm() {
-    var num = document.getElementById("numEtu").value;
+    const num = numEtuInput.value;
     localStorage.setItem("NumEtu", num);
 
     $.ajax({
@@ -27,11 +29,26 @@ function submitForm() {
             }
         }
     });
+
+    /* fetch(`/checkNumEtu.php?num_etu=${num}`)
+        .then(response => response.text())
+        .then(message => {
+            if (message == num + " authorized") {
+                document.getElementById("sendID").submit();
+                $(".input").css("background-color", "#2BE8C6");
+                $("#message").html("<i class='fas fa-check'></i> Ce numéro existe !");
+            } else {
+                $(".input").css("background-color", "#FE4C6A");
+                $("#message").html("<i class='fas fa-times'></i> Ce numéro n'existe pas !");
+            }
+        })
+        .catch(error => console.error(error)) */
 }
 
-document.onkeydown = function (e) {
+
+numEtuInput.addEventListener('keydown', function (e) {
     if (e.key === "Enter") {
         e.preventDefault();
         submitForm();
     }
-}
+})
