e0e53c66163e44860baf1a95546808d764fc7ab4
QuentiumYT <quentin.lienhardt@gmail.com>
2020-09-04 18:39:55 +0200

    Added footer back

--- assets/include/soon.php
+++ assets/include/soon.php
@@ -1,4 +1,19 @@
 <?php
 echo "<h3>Le site NoteUniv sera de retour une fois que des notes seront rentrées ! Merci de toujours utiliser ce service, ça nous fais vraiment plaisir ♥ À bientôt !</h3>";
+?>
 
+<footer>
+    <div class="row center-xs">
+        <div class="col-xs-12">
+            <p class="as-small">Made with ❤️ by <a href="https://erosya.fr" target="_BLANK">Erosya</a> | <span class="tippy-note" data-tippy-content="Discord: Ynohtna#0001 / QuentiumYT#0207 | contact@anthony-adam.fr / support@quentium.fr">Nous contacter</span> | <a href="terms.html">Mentions légales</a></p>
+        </div>
+    </div>
+    <!-- SCRIPT EXT -->
+    <script src="https://unpkg.com/@popperjs/core"></script>
+    <script src="https://unpkg.com/tippy.js"></script>
+    <!-- SCRIPT PERSO -->
+    <script src="assets/js/app.js"></script>
+</footer>
+
+<?php
 exit();
